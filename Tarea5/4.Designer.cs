bf2a366870ef0678ec044e8dc0d3a61658253603
josedavid980111 <35538108+josedavid980111@users.noreply.github.com>
2019-03-16 20:50:09 -0600

    Modificacion Función

--- Tarea5/4.Designer.cs
+++ Tarea5/4.Designer.cs
@@ -30,38 +30,85 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(61, 54);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(60, 113);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(650, 91);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Función Cantidad de Productos en una Sucursal";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(61, 217);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(60, 307);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(651, 91);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Procedimiento: Sucursales Con Producto";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(179, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(513, 41);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(88, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Cadena";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(415, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sucursal";
             // 
             // _4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "_4";
             this.Text = "_4";
             this.Load += new System.EventHandler(this._4_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,5 +116,10 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
--- Tarea5/4.cs
+++ Tarea5/4.cs
@@ -13,22 +13,26 @@ namespace Tarea5
 {
     public partial class _4 : Form
     {
-
+        //Atributos
+        GestorBD.GestorBD GestorBD;
         OleDbConnection cnOracle;
+        Varios.Comunes comunes = new Varios.Comunes();
+        String cadSQL;
+        DataSet dsCadena = new DataSet(), dsSucursal = new DataSet();
 
         public _4()
         {
             InitializeComponent();
         }
-
-        private void _4_Load(object sender, EventArgs e)
-        {
-
-        }
-
+        
         private void button1_Click(object sender, EventArgs e)
         {
             String NombreCadena, NombreSucursal;
+            //1- Hcer la conexión a la BD de Oracle
+            cnOracle = new OleDbConnection("Provider=MSDAORA; Data Source=xe;" +
+              "User ID=system;Password=gonbar");
+            cnOracle.Open();
+
             OleDbCommand procAlmacenado;
             OleDbParameter salida, parametro1, parametro2;
             int cant;
@@ -40,25 +44,27 @@ namespace Tarea5
             procAlmacenado = new OleDbCommand();
             procAlmacenado.Connection = cnOracle;
 
-            //2- Especificar el llamado al procedimiento  (en general: al subprograma).
+            //2- Especificar el llamado a la función  (en general: al subprograma).
             procAlmacenado.CommandText = "CantidadProductos";
             procAlmacenado.CommandType = CommandType.StoredProcedure;
-
-            //b) primero todos los de salida (uno en este caso):
+            
+            
+            //3- Especificar los parámetros:
+            //a) primero todos los de salida (uno en este caso):
             salida = new OleDbParameter("RETURN_VALUE", OleDbType.Integer,
               4, ParameterDirection.ReturnValue, false, 4, 0, "NombreCadena" + "NombreSucursal", DataRowVersion.Current, 0);
             procAlmacenado.Parameters.Add(salida);
 
-            //3- Especificar los parámetros:
-            //a) Luego todos los de entrada:
-            NombreCadena = "CC1";
+
+            //b) Luego todos los de entrada:
+            NombreCadena = comboBox1.SelectedItem.ToString();
             parametro1 = new OleDbParameter("NombreCad", NombreCadena);
-            NombreSucursal = "Sucursal1_CC1";
+            NombreSucursal = comboBox2.SelectedItem.ToString();
             parametro2 = new OleDbParameter("NombreSuc", NombreSucursal);
             procAlmacenado.Parameters.Add(parametro1);
             procAlmacenado.Parameters.Add(parametro2);
 
-            //4- Ejecutar el procedimiento (en general: el subprograma).
+            //4- Ejecutar la función (en general: el subprograma).
             try
             {
                 procAlmacenado.ExecuteNonQuery();
@@ -73,9 +79,38 @@ namespace Tarea5
             {
                 MessageBox.Show(err.Message);
             }
-
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
             //6- Cerrar la conexión a la BD.
             cnOracle.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void _4_Load(object sender, EventArgs e)
+        {
+            //1- Hcer la conexión a la BD de Oracle
+
+            GestorBD = new GestorBD.GestorBD("MSDAORA", "System", "gonbar", "xe");
+
+            //2.1- Obtiene y muestra los datos de los alumnos.
+            cadSQL = "Select * from T4CADCOM";
+            GestorBD.consBD(cadSQL, dsCadena, "TablaCadena");
+
+
+            comunes.cargaCombo(comboBox1, dsCadena, "TablaCadena", "NombreCadena");
+
+            //2.2- Obtiene y muestra los datos de los materia.
+            cadSQL = "Select * from T4Sucursal";
+            GestorBD.consBD(cadSQL, dsSucursal, "TablaSucursal");
+
+
+            comunes.cargaCombo(comboBox2, dsSucursal, "TablaSucursal", "NombreSucursal");
+
         }
     }
 }
