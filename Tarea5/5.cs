dc371c168b4190ab7aca02ff15f5b5c5aa07c7a4
josedavid980111 <35538108+josedavid980111@users.noreply.github.com>
2019-03-20 18:29:13 -0600

    Listo Final

--- Tarea5/5.cs
+++ Tarea5/5.cs
@@ -24,79 +24,103 @@ namespace Tarea5
 
         private void cbCliente_SelectedIndexChanged(object sender, EventArgs e)
         {
-            String fecha = dtp5.Value.Year + "-" + dtp5.Value.Month + "-" + dtp5.Value.Day;
-            String nombre = cbCliente.SelectedItem.ToString();
+            try
+            {
+                String fecha = dtp5.Value.Year + "-" + dtp5.Value.Month + "-" + dtp5.Value.Day;
+                String nombre = cbCliente.SelectedItem.ToString();
 
 
-            //2.2- Obtiene y muestra Folios de las facturas del cliente.
+                //2.2- Obtiene y muestra Folios de las facturas del cliente.
 
-            cadSQL = "select f.folio from t4factura f, t4cliente c where f.RFCCliente=c.RFCCliente and c.nombrecliente='" + nombre + "' and f.fecha between date'" + fecha + "' and sysdate";
+                cadSQL = "select f.folio from t4factura f, t4cliente c where f.RFCCliente=c.RFCCliente and c.nombrecliente='" + nombre + "' and f.fecha between date'" + fecha + "' and sysdate";
 
-            GestorBD.consBD(cadSQL, dsFactura, "TablaFactura");
-            comunes.cargaCombo(cboNoFactura, dsFactura, "TablaFactura", "Folio");
-        }
+                GestorBD.consBD(cadSQL, dsFactura, "TablaFactura");
+                comunes.cargaCombo(cboNoFactura, dsFactura, "TablaFactura", "Folio");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+}
 
         DataSet dsActual = new DataSet();
 
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
-            String fecha = dtp5.Value.Year + "-" + dtp5.Value.Month + "-" + dtp5.Value.Day;
-            String nombre = cbCliente.SelectedItem.ToString();
+            try
+            {
+                String fecha = dtp5.Value.Year + "-" + dtp5.Value.Month + "-" + dtp5.Value.Day;
+                String nombre = cbCliente.SelectedItem.ToString();
 
 
 
-            //2.2- Obtiene y muestra Folios de las facturas del cliente.
+                //2.2- Obtiene y muestra Folios de las facturas del cliente.
 
-            cadSQL = "select f.folio from t4factura f, t4cliente c where f.RFCCliente=c.RFCCliente and c.nombrecliente='" + nombre + "' and f.fecha between date'" + fecha + "' and sysdate";
+                cadSQL = "select f.folio from t4factura f, t4cliente c where f.RFCCliente=c.RFCCliente and c.nombrecliente='" + nombre + "' and f.fecha between date'" + fecha + "' and sysdate";
 
-            GestorBD.consBD(cadSQL, dsFactura, "TablaFactura");
-            comunes.cargaCombo(cboNoFactura, dsFactura, "TablaFactura", "Folio");
-            cboNoFactura.SelectedIndex = -1;
+                GestorBD.consBD(cadSQL, dsFactura, "TablaFactura");
+                comunes.cargaCombo(cboNoFactura, dsFactura, "TablaFactura", "Folio");
+                cboNoFactura.SelectedIndex = -1;
+                cboNoFactura.Text = "";
+            }
+            
+        
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
 
 
-        }
+}
         private void cboNoFactura_SelectedIndexChanged(object sender, EventArgs e)
         {
-            String fecha = dtp5.Value.Year + "-" + dtp5.Value.Month + "-" + dtp5.Value.Day;
-            String nombre = cbCliente.SelectedItem.ToString();
-            int folio = Convert.ToInt16(cboNoFactura.Text);
+            try
+            {
+                String fecha = dtp5.Value.Year + "-" + dtp5.Value.Month + "-" + dtp5.Value.Day;
+                String nombre = cbCliente.SelectedItem.ToString();
+                int folio = Convert.ToInt16(cboNoFactura.Text);
 
-            //muestra las compras de la factura en el datagridView
-            cadSQL = "select fac.fecha, prod.nombrep as Articulo, con.preciototalarticulo as Total_Articulo from " +
-                "t4contiene con, t4factura fac, t4cliente cli, t4producto prod" +
-                " where cli.nombrecliente ='" + nombre + "' and fac.rfccliente = cli.rfccliente  and con.folio = " + folio +
-                " and fac.fecha between date'" + fecha + "' and sysdate and prod.idprod = con.idprod order by con.folio desc";
-            GestorBD.consBD(cadSQL, dsFactura, "factura");
-            dtgFac.DataSource = dsFactura.Tables["factura"];
-
-
-            //muestra el total en label de total
-            cadSQL = "select sum(con.preciototalarticulo) as Total from " +
-                "t4contiene con, t4factura fac, t4cliente cli, t4producto prod" + "" +
-                " where cli.nombrecliente ='" + nombre + "' and fac.rfccliente = cli.rfccliente  and con.folio = " + folio +
-                " and fac.fecha between date'" + fecha + "' and sysdate and prod.idprod = con.idprod order by con.folio desc";
-            GestorBD.consBD(cadSQL, dsTotal, "factura");
-            Double Total = Convert.ToDouble(dsTotal.Tables["factura"].Rows[0]["Total"]);
-            lblTotal.Text = Total.ToString();
+                //muestra las compras de la factura en el datagridView
+                cadSQL = "select fac.fecha, prod.nombrep as Articulo, con.preciototalarticulo as Total_Articulo from " +
+                    "t4contiene con, t4factura fac, t4cliente cli, t4producto prod" +
+                    " where cli.nombrecliente ='" + nombre + "' and fac.rfccliente = cli.rfccliente  and con.folio = " + folio +
+                    " and fac.fecha between date'" + fecha + "' and sysdate and prod.idprod = con.idprod order by con.folio desc";
+                GestorBD.consBD(cadSQL, dsFactura, "factura");
+                dtgFac.DataSource = dsFactura.Tables["factura"];
 
 
-            //muestra el numero de pagos  hechos
-            cadSQL = "select count(*) from t4pagos where folio = " + folio;
-            GestorBD.consBD(cadSQL, dsPagos, "pagos");
-            lblPagosR.Text = dsPagos.Tables["pagos"].Rows[0][0].ToString();
+                //muestra el total en label de total
+                cadSQL = "select sum(con.preciototalarticulo) as Total from " +
+                    "t4contiene con, t4factura fac, t4cliente cli, t4producto prod" + "" +
+                    " where cli.nombrecliente ='" + nombre + "' and fac.rfccliente = cli.rfccliente  and con.folio = " + folio +
+                    " and fac.fecha between date'" + fecha + "' and sysdate and prod.idprod = con.idprod order by con.folio desc";
+                GestorBD.consBD(cadSQL, dsTotal, "factura");
+                Double Total = Convert.ToDouble(dsTotal.Tables["factura"].Rows[0]["Total"]);
+                lblTotal.Text = Total.ToString();
 
-            //muestra el monto total de los pagos
-            cadSQL = "select sum(monto) as total from t4pagos where folio = " + folio;
-            GestorBD.consBD(cadSQL, dsMonto, "monto");
 
-            Double Pagado = Convert.ToDouble(dsMonto.Tables["monto"].Rows[0]["total"]);
-            lblMontoPagos.Text = Pagado.ToString();
+                //muestra el numero de pagos  hechos
+                cadSQL = "select count(*) from t4pagos where folio = " + folio;
+                GestorBD.consBD(cadSQL, dsPagos, "pagos");
+                lblPagosR.Text = dsPagos.Tables["pagos"].Rows[0][0].ToString();
 
-            Double totalPagos = Convert.ToDouble(Pagado);
+                //muestra el monto total de los pagos
+                cadSQL = "select sum(monto) as total from t4pagos where folio = " + folio;
+                GestorBD.consBD(cadSQL, dsMonto, "monto");
 
-            //muestra el saldo Actual de la factura
-            lblSaldo.Text = Convert.ToString(Total - Pagado);
+                Double Pagado = Convert.ToDouble(dsMonto.Tables["monto"].Rows[0]["total"]);
+                lblMontoPagos.Text = Pagado.ToString();
 
+                Double totalPagos = Convert.ToDouble(Pagado);
+
+                //muestra el saldo Actual de la factura
+                lblSaldo.Text = Convert.ToString(Total - Pagado);
+            }
+            
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
 
         }
 
